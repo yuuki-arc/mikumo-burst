0f9088b73d184593bf8bab122ecfd16e21bff81e
yuuki-arc <arcs7nine@gmail.com>
2014-08-28 03:47:38 +0900

    CocosBuilder対応（Android側）

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -10,16 +10,22 @@ LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/TitleScene.cpp \
-                   ../../Classes/HomeScene.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+#LOCAL_SRC_FILES := hellocpp/main.cpp \
+#                   ../../Classes/AppDelegate.cpp \
+#                   ../../Classes/TitleScene.cpp \
+#                   ../../Classes/HomeScene.cpp \
+#                   ../../Classes/HelloWorldScene.cpp
+CPP_FILES := $(shell find $(LOCAL_PATH)/../../Classes -name *.cpp)
+ 
+LOCAL_SRC_FILES := hellocpp/main.cpp
+LOCAL_SRC_FILES += $(CPP_FILES:$(LOCAL_PATH)/%=%)
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocosbuilder_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
 
 # LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += cocosbuilder_static
@@ -33,6 +39,8 @@ include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,.)
 $(call import-module,audio/android)
+$(call import-module,editor-support/cocosbuilder)
+$(call import-module,extensions)
 
 # $(call import-module,Box2D)
 # $(call import-module,editor-support/cocosbuilder)
