cdd2968a6ba1879cbd0eec28b40c50ff32ace923
yuuki-arc <arcs7nine@gmail.com>
2014-09-07 23:10:42 +0900

    定数定義を追加

---
+++ Classes/Constant.cpp
@@ -0,0 +1,4 @@
+#include "Constant.h"
+
+const int LIMIT_RANK = 99;
+const int LIMIT_DAMAGE = 999999;
---
+++ Classes/Constant.h
@@ -0,0 +1,10 @@
+#ifndef __Fragment_of_Eternity__Constant__
+#define __Fragment_of_Eternity__Constant__
+
+struct Constant
+{
+    static const int LIMIT_RANK;
+    static const int LIMIT_DAMAGE;
+};
+
+#endif /* defined(__Fragment_of_Eternity__Constant__) */
