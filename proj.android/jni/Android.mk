b514e5949d756fbe86fe5664d7a1ce9356baac65
yuuki-arc <arcs7nine@gmail.com>
2014-11-15 21:28:54 +0900

    AndroidのappC cloud設定でspine/Json.hを読み込めるようにする

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -25,6 +25,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosbuilder_static
+LOCAL_WHOLE_STATIC_LIBRARIES += spine_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
 
 # LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
@@ -40,6 +41,7 @@ include $(BUILD_SHARED_LIBRARY)
 $(call import-module,.)
 $(call import-module,audio/android)
 $(call import-module,editor-support/cocosbuilder)
+$(call import-module,editor-support/spine)
 $(call import-module,extensions)
 
 # $(call import-module,Box2D)
