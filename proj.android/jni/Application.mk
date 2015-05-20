14b5e09b2f4a66ac8cbde54b1e796cc16cd5c8f3
yuuki-arc <arcs7nine@gmail.com>
2015-05-21 03:52:18 +0900

    add misc

--- proj.android/jni/Application.mk
+++ proj.android/jni/Application.mk
@@ -4,6 +4,7 @@ NDK_TOOLCHAIN_VERSION=clang
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char -U__SSE__
 APP_LDFLAGS := -latomic
 APP_ABI := armeabi armeabi-v7a x86
+#APP_ABI := x86
 
 APP_DEBUG := $(strip $(NDK_DEBUG))
 ifeq ($(APP_DEBUG),1)
